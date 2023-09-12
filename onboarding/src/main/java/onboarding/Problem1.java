89f7be173aa10c10615cd01be538bbec9796cda8
junest66 <chlwndks333@gmail.com>
2023-09-13 02:08:20 +0900

    docs: Javadoc 주석 포맷 수정

--- onboarding/src/main/java/onboarding/Problem1.java
+++ onboarding/src/main/java/onboarding/Problem1.java
@@ -5,7 +5,7 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
-        if(!validate(pobi) || !validate(crong)) {
+        if (!validate(pobi) || !validate(crong)) {
             return -1;
         }
         int pobiValue = getMaxValue(pobi);
@@ -17,26 +17,27 @@ class Problem1 {
 
     /**
      * 번호 검증하는 함수
+     *
      * @param numbers
      * @return
      */
     public static boolean validate(List<Integer> numbers) {
-        if(numbers.size() != 2) {
+        if (numbers.size() != 2) {
             return false;
         }
 
         int firstNumber = numbers.get(0);
         int secondNumber = numbers.get(1);
 
-        if(!checkRange(firstNumber) || !checkRange(secondNumber)) {
+        if (!checkRange(firstNumber) || !checkRange(secondNumber)) {
             return false;
         }
 
-        if(firstNumber % 2 != 1 || secondNumber % 2 != 0) {
+        if (firstNumber % 2 != 1 || secondNumber % 2 != 0) {
             return false;
         }
 
-        if(secondNumber - firstNumber != 1) {
+        if (secondNumber - firstNumber != 1) {
             return false;
         }
         return true;
@@ -44,11 +45,12 @@ class Problem1 {
 
     /**
      * 범호 범위 확인하는 함수
+     *
      * @param number
      * @return
      */
     public static boolean checkRange(int number) {
-        if(number < 1 || number > 400) {
+        if (number < 1 || number > 400) {
             return false;
         }
         return true;
@@ -56,24 +58,26 @@ class Problem1 {
 
     /**
      * 가장 큰 값을 구하는 함수
+     *
      * @param numbers
      * @return
      */
     public static int getMaxValue(List<Integer> numbers) {
-        int leftMaxValue = compare(getSumValue(numbers.get(0)),getMultiplyValue(numbers.get(0)));
-        int rightMaxValue = compare(getSumValue(numbers.get(1)),getMultiplyValue(numbers.get(1)));
+        int leftMaxValue = compare(getSumValue(numbers.get(0)), getMultiplyValue(numbers.get(0)));
+        int rightMaxValue = compare(getSumValue(numbers.get(1)), getMultiplyValue(numbers.get(1)));
 
         return compare(leftMaxValue, rightMaxValue);
     }
 
     /**
      * 자리수의 덧셈 값을 구하는 함수
+     *
      * @param number
      * @return
      */
     public static int getSumValue(int number) {
         int sum = 0;
-        while(number != 0) {
+        while (number != 0) {
             sum += number % 10;
             number /= 10;
         }
@@ -82,13 +86,14 @@ class Problem1 {
 
     /**
      * 각 자리수의 곱셈값 구하는 함수
+     *
      * @param number
      * @return
      */
     public static int getMultiplyValue(int number) {
         int value = 1;
 
-        while(number != 0) {
+        while (number != 0) {
             value *= number % 10;
             number /= 10;
         }
@@ -98,6 +103,7 @@ class Problem1 {
 
     /**
      * 두수를 비교하는 함수
+     *
      * @param sum
      * @param multiply
      * @return
@@ -108,6 +114,7 @@ class Problem1 {
 
     /**
      * 결과값 리턴하는 함수
+     *
      * @param pobiValue
      * @param crongValue
      * @return
