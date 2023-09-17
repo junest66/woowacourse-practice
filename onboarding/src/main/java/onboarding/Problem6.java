624b5eaed55fa42ffcae017bcd4374773fad7e33
junest66 <chlwndks333@gmail.com>
2023-09-17 22:52:47 +0900

    feat: Problem5.java email validation function added

--- onboarding/src/main/java/onboarding/Problem6.java
+++ onboarding/src/main/java/onboarding/Problem6.java
@@ -1,10 +1,19 @@
 package onboarding;
 
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
+    }
+
+    /**
+     * 이메일 형식 검증하는 함수
+     */
+    public static boolean isVaildEmail(String email) {
+        String emailPattern = "\\w{1,10}@email\\.com";
+        return Pattern.matches(emailPattern, email);
     }
 }
