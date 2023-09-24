cb8166e3dc5e123652a5105b8964e1cdbf70a39c
junest66 <chlwndks333@gmail.com>
2023-09-24 16:55:29 +0900

    style : 출력문 위치 이동

--- java-baseball/src/main/java/baseball/controller/GameController.java
+++ java-baseball/src/main/java/baseball/controller/GameController.java
@@ -23,9 +23,9 @@ public class GameController {
     }
 
     public void gameStart() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
         do {
             initComputer();
-            System.out.println("숫자 야구 게임을 시작합니다.");
             startGame();
         } while (!restartGame());
     }
