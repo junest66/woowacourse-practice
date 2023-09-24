2ab242c1bbb6dfdf81f4ad44e5cde7973bf1bd53
junest66 <chlwndks333@gmail.com>
2023-09-24 16:55:15 +0900

    style : 출력문 형식 수정

--- java-baseball/src/main/java/baseball/model/Referee.java
+++ java-baseball/src/main/java/baseball/model/Referee.java
@@ -41,8 +41,6 @@ public class Referee {
 
     public void judge(List<Integer> user, List<Integer> computer) {
 
-        System.out.println("user.toString() = " + user.toString());
-        System.out.println("computer = " + computer.toString());
         //같은자리애 같은수 (스트라이크)
         int strike = 0;
         for (int i = 0; i < user.size(); i++) {
@@ -50,7 +48,6 @@ public class Referee {
                 strike++;
             }
         }
-        System.out.println("strike = " + strike);
         setStrikeNum(strike);
 
         //Set ( 볼 + 스트라이크 )
@@ -60,19 +57,22 @@ public class Referee {
 
         //Set - 같은자리에 같은수 (볼)
         setBolNum(setUser.size() - strike);
-        System.out.println("setUser size = " + setUser.size());
     }
 
     public void print() {
+        if (bolNum > 0) {
+            System.out.print(bolNum + "볼 ");
+        }
+        if (strikeNum > 0) {
+            System.out.print(strikeNum + "스트라이크");
+        }
+        if (bolNum == 0 && strikeNum == 0) {
+            System.out.print("낫싱");
+        }
+        System.out.println("");
+
         if (strikeNum == 3) {
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
             success = true;
-        } else if (bolNum > 0) {
-            System.out.println(bolNum + "볼");
-        } else if (strikeNum > 0) {
-            System.out.println(strikeNum + "스트라이크");
-        } else if (bolNum == 0 && strikeNum == 0) {
-            System.out.println("낫싱");
-        }
-    }
+        }}
 }
