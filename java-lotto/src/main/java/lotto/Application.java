0e178e7efae7ba5bbbdf74b64a58671901fb329e
junest66 <chlwndks333@gmail.com>
2023-09-28 02:42:03 +0900

    feat: Application playLotto 실행

--- java-lotto/src/main/java/lotto/Application.java
+++ java-lotto/src/main/java/lotto/Application.java
@@ -1,7 +1,11 @@
 package lotto;
 
+import lotto.controller.UserController;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        UserController userController = new UserController();
+        userController.playLotto();
     }
 }
