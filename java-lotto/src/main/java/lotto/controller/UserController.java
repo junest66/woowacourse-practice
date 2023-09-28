1ad04811b10bd89d5e966d8a06f912a7dabc8675
junest66 <chlwndks333@gmail.com>
2023-09-28 20:31:55 +0900

    feat: UserController playLotto() try-catch문 적용

--- java-lotto/src/main/java/lotto/controller/UserController.java
+++ java-lotto/src/main/java/lotto/controller/UserController.java
@@ -1,5 +1,6 @@
 package lotto.controller;
 
+import lotto.constant.ErrorOutputMessage;
 import lotto.model.User;
 import camp.nextstep.edu.missionutils.Console;
 
@@ -12,24 +13,34 @@ public class UserController {
     }
 
     public void playLotto() {
-        purchase();
-        user.printLotto();
-        inputWinningNumber();
-        user.checkWinning();
-        user.printResult();
-        user.getRate();
+        try {
+            purchase();
+            user.printLotto();
+            inputWinningNumber();
+            user.checkWinning();
+            user.printResult();
+            user.getRate();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
-
     private void purchase() {
         System.out.println("구입금액 입력 해주세요");
-        user.setLottoList(Integer.parseInt(Console.readLine())); //NullFormat 예외처리해야함
+        try {
+            user.setLottoList(Integer.parseInt(Console.readLine()));
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ErrorOutputMessage.PURCHASE_TYPE);
+        }
     }
 
     private void inputWinningNumber() {
-        System.out.println("당첨 번호를 입력해 주세요");
-        user.setWinningLotto(Console.readLine());
+        try {
+            System.out.println("당첨 번호를 입력해 주세요");
+            user.setWinningLotto(Console.readLine());
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException(ErrorOutputMessage.PURCHASE_TYPE);
+        }
         System.out.println("보너스 번호를 입력해 주세요");
         user.setBonusNumber(Integer.parseInt(Console.readLine()));
     }
-
 }
