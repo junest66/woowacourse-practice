7467ebfb769411d55c2c7303fde6eb31b3ff4242
junest66 <chlwndks333@gmail.com>
2023-09-28 20:32:49 +0900

    feat: Lotto maxNumber, minNumber 상수 추가 및 예외처리

--- java-lotto/src/main/java/lotto/model/Lotto.java
+++ java-lotto/src/main/java/lotto/model/Lotto.java
@@ -1,9 +1,13 @@
 package lotto.model;
 
+import lotto.constant.ErrorOutputMessage;
+
 import java.util.*;
 
 public class Lotto {
     public static final int LOTTO_PRICE = 1000;
+    public static final int LOTTO_MIN_NUMBER = 1;
+    public static final int LOTTO_MAX_NUMBER = 45;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -13,18 +17,18 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ErrorOutputMessage.LOTTO_LENGTH);
         }
 
         Set<Integer> numberSet = new HashSet<>(numbers);
 
         if(numbers.size() != numberSet.size()) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ErrorOutputMessage.LOTTO_DUPLICATED);
         }
 
         for (Integer number : numbers) {
             if(number < 1 || number > 45) {
-                throw new IllegalArgumentException();
+                throw new IllegalArgumentException(ErrorOutputMessage.LOTTO_RANGE);
             }
         }
     }
