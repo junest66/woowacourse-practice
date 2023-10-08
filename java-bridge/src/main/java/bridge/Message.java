5cee57eea98829d3abb96be7fd99f7ca844e9852
junest66 <chlwndks333@gmail.com>
2023-10-09 02:34:29 +0900

    feat: Message 상수 클래스 추가

---
+++ java-bridge/src/main/java/bridge/Message.java
@@ -0,0 +1,13 @@
+package bridge;
+
+public final class Message {
+
+    public static final String START_PRINT = "다리 건너기 게임을 시작합니다.";
+    public static final String INPUT_BRIDGE = "다리의 길이를 입력해주세요.";
+    public static final String MOVE_BRIDGE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    public static final String GAME_RESTART = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+    public static final String RESULT_GAME_MESSAGE = "최종 게임 결과";
+    public static final String GAME_SUCCESS_STATUS = "게임 성공 여부: ";
+    public static final String TOTAL_GAME_NUMBER = "총 시도한 횟수: ";
+
+}
