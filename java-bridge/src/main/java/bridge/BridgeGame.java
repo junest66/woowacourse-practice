568512ef3b3c1a1876429df31e1e91e9d175c2d9
junest66 <chlwndks333@gmail.com>
2023-10-11 01:38:38 +0900

    refactor: BridgeGame move(), successTurn(), failTurn() 추가 및 함수 분리

--- java-bridge/src/main/java/bridge/BridgeGame.java
+++ java-bridge/src/main/java/bridge/BridgeGame.java
@@ -24,8 +24,39 @@ public class BridgeGame {
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public void move(boolean result, String input) {
+        if(result) {
+            successTurn(input);
+        }
+        if(!result) {
+            failTurn(input);
+        }
+
     }
+
+    public void successTurn(String input) {
+        if (input.equals("U")) {
+            currentUpMap.add("O");
+            currentDownMap.add(" ");
+        }
+        if (input.equals("D")) {
+            currentDownMap.add("O");
+            currentUpMap.add(" ");
+        }
+    }
+
+    public void failTurn(String input) {
+        if (input.equals("U")) {
+            currentUpMap.add("X");
+            currentDownMap.add(" ");
+        }
+        if (input.equals("D")) {
+            currentDownMap.add("X");
+            currentUpMap.add(" ");
+        }
+    }
+
+
 
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
@@ -34,14 +65,6 @@ public class BridgeGame {
      */
     public void retry() {
         this.gameStatus = false;
-    }
-
-    public void addCurrentUpMap(String input) {
-        currentUpMap.add(input);
-    }
-
-    public void addCurrentDownMap(String input) {
-        currentDownMap.add(input);
     }
 
     public boolean getGameStatus() {
