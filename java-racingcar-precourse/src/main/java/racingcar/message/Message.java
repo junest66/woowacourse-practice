45b6ca5c9b84e1a20220b17ec1120d05e13c0696
junest66 <chlwndks333@gmail.com>
2023-10-19 01:18:48 +0900

    feat: Message 상수 추가

---
+++ java-racingcar-precourse/src/main/java/racingcar/message/Message.java
@@ -0,0 +1,12 @@
+package racingcar.message;
+
+public class Message {
+
+    public static final String CAR_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String COUNT_INPUT_MESSAGE = "시도할 회수는 몇회인가요?";
+    public static final String RESULT_MESSAGE = "실행결과";
+    public static final String FINAL_WINNER = "최종 우승자 : ";
+    public static final String CAR_NAME_INPUT_ERROR_MESSAGE = "[ERROR] 자동차의 이름은 5자 이하만 가능하다.";
+    public static final String COUNT_INPUT_ERROR_MESSAGE = "[ERROR] 시도 횟수는 숫자여야 한다.";
+
+}
