911ee92675a17fce1621d4a360b8858f571cfa08
junest66 <chlwndks333@gmail.com>
2023-11-13 02:33:49 +0900

    feat: Messages, Values 상수 추가

---
+++ src/main/java/christmas/constants/Values.java
@@ -0,0 +1,56 @@
+package christmas.constants;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Values {
+    public static final String CHRISTMAS_EVENT_NAME = "크리스마스 디데이 할인";
+    public static final String WEEKDAY_EVENT_NAME = "평일 할인";
+    public static final String WEEKEND_EVENT_NAME = "주말 할인";
+    public static final String GIVEAWAY_EVENT_NAME = "증정 이벤트";
+    public static final String SPECIAL_EVENT_NAME = "특별 할인";
+    public static final String BADGE_SANTA = "산타";
+    public static final String BADGE_TREE = "트리";
+    public static final String BADGE_STAR = "별";
+    public static final String NOT_EXIST = "없음";
+    public static final String GIVEAWAY_EVENT_ITEMS = "샴페인 1개";
+    public static final String ITEM_SEPARATOR = ",";
+    public static final String MENU_QUANTITY_SEPARATOR = "-";
+    public static final String MENU_ITEM_PATTERN = "[가-힣a-zA-Z]+-\\d+";
+
+
+    public static final List<Integer> starDay = Arrays.asList(3, 10, 17, 24, 25, 31);
+    public static final int MINIMUM_TOTAL_ORDER_AMOUNT_FOR_EVENT = 10000;
+    public static final int MINIMUM_TOTAL_ORDER_AMOUNT_FOR_GIVEAWAY_EVENT = 12000;
+    public static final int MIN_DATE = 1;
+    public static final int MAX_DATE = 31;
+    public static final int MINIMUM_DATE_FOR_EVENT = 1;
+    public static final int MAXIMUM_DATE_FOR_EVENT = 31;
+    public static final int MAXIMUM_DATE_FOR_CHRISTMAS_EVENT = 25;
+    public static final int CHRISTMAS_EVENT_START_DISCOUNT = 1000;
+    public static final int CHRISTMAS_EVENT_DAILY_ADDITION = 100;
+    public static final int WEEKDAY_DISCOUNT_AMOUNT = 2023;
+    public static final int WEEKEND_DISCOUNT_AMOUNT = 2023;
+    public static final int SPECIAL_EVENT_DISCOUNT_AMOUNT = 1000;
+    public static final int DAYS_IN_WEEK = 7;
+    public static final int WEEKEND_DAY_FRIDAY = 1;
+    public static final int WEEKEND_DAY_SATURDAY = 2;
+    public static final int MUSHROOM_SOUP_PRICE = 6000;
+    public static final int TAPAS_PRICE = 5500;
+    public static final int CAESAR_SALAD_PRICE = 8000;
+    public static final int TBONE_STEAK_PRICE = 55000;
+    public static final int BBQ_RIBS_PRICE = 54000;
+    public static final int SEAFOOD_PASTA_PRICE = 35000;
+    public static final int CHRISTMAS_PASTA_PRICE = 25000;
+    public static final int CHOCO_CAKE_PRICE = 15000;
+    public static final int ICE_CREAM_PRICE = 5000;
+    public static final int ZERO_COLA_PRICE = 3000;
+    public static final int RED_WINE_PRICE = 60000;
+    public static final int CHAMPAGNE_PRICE = 25000;
+    public static final int BADGE_SANTA_REQUIREMENT = 20000;
+    public static final int BADGE_TREE_REQUIREMENT = 10000;
+    public static final int BADGE_STAR_REQUIREMENT = 5000;
+
+
+
+}
