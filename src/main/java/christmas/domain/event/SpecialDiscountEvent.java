8446daca89c473980c92162cffe6ca561ad8996a
junest66 <chlwndks333@gmail.com>
2023-11-13 03:03:49 +0900

    feat(GiveawayEvent): 특별할인이벤트를 관리하는 클래스 추가

---
+++ src/main/java/christmas/domain/event/SpecialDiscountEvent.java
@@ -0,0 +1,39 @@
+package christmas.domain.event;
+
+import christmas.constants.Values;
+import christmas.domain.Order;
+
+public class SpecialDiscountEvent implements Event {
+    private final Order order;
+
+    public SpecialDiscountEvent(Order order) {
+        this.order = order;
+    }
+
+    @Override
+    public String getEventName() {
+        return Values.SPECIAL_EVENT_NAME;
+    }
+
+    @Override
+    public boolean isNotApplicable() {
+        if (order.calculateTotalPriceBeforeDiscount() < Values.MINIMUM_TOTAL_ORDER_AMOUNT_FOR_EVENT) {
+            return true;
+        }
+        if (order.getDate() < Values.MINIMUM_DATE_FOR_EVENT || order.getDate() > Values.MAXIMUM_DATE_FOR_EVENT) {
+            return true;
+        }
+        if (!Values.starDay.contains(order.getDate())) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public int calculateDiscount() {
+        if (isNotApplicable()) {
+            return 0;
+        }
+        return Values.SPECIAL_EVENT_DISCOUNT_AMOUNT;
+    }
+}
