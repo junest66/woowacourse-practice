1af66c66e021811ce63d49775137f55271d53e8e
junest66 <chlwndks333@gmail.com>
2023-11-13 03:02:28 +0900

    feat(GiveawayEvent): 증정이벤트를 관리하는 클래스 추가

---
+++ src/main/java/christmas/domain/event/GiveawayEvent.java
@@ -0,0 +1,36 @@
+package christmas.domain.event;
+
+import christmas.constants.Values;
+import christmas.domain.Order;
+
+public class GiveawayEvent implements Event {
+    private final Order order;
+
+    @Override
+    public String getEventName() {
+        return Values.GIVEAWAY_EVENT_NAME;
+    }
+
+    public GiveawayEvent(Order order) {
+        this.order = order;
+    }
+
+    @Override
+    public boolean isNotApplicable() {
+        if (order.getDate() < Values.MINIMUM_DATE_FOR_EVENT || order.getDate() > Values.MAXIMUM_DATE_FOR_EVENT) {
+            return true;
+        }
+        if (order.calculateTotalPriceBeforeDiscount() < Values.MINIMUM_TOTAL_ORDER_AMOUNT_FOR_GIVEAWAY_EVENT) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public int calculateDiscount() {
+        if (isNotApplicable()) {
+            return 0;
+        }
+        return Values.CHAMPAGNE_PRICE;
+    }
+}
