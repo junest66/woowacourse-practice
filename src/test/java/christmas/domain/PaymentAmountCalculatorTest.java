ac97f2a7106e98c695e5b11d4718054f0ef3e0e1
junest66 <chlwndks333@gmail.com>
2023-11-13 19:08:06 +0900

    test(PaymentAmountCalculatorTest): 지불금액을 계산하는 기능 테스트 추가

---
+++ src/test/java/christmas/domain/PaymentAmountCalculatorTest.java
@@ -0,0 +1,40 @@
+package christmas.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import christmas.constants.MenuItem;
+import christmas.constants.Values;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class PaymentAmountCalculatorTest {
+    private Order order;
+    private DiscountResult discountResult = new DiscountResult();
+
+    @BeforeEach
+    void setup() {
+        Map<MenuItem, Integer> menuOrder = new HashMap<>();
+        menuOrder.put(MenuItem.티본스테이크, 1);
+        menuOrder.put(MenuItem.바비큐립, 1);
+        menuOrder.put(MenuItem.초코케이크, 2);
+        menuOrder.put(MenuItem.제로콜라, 1);
+        order = new Order(3, menuOrder);
+        discountResult.addDiscount(Values.CHRISTMAS_EVENT_NAME, -1200);
+        discountResult.addDiscount(Values.WEEKDAY_EVENT_NAME, -4046);
+        discountResult.addDiscount(Values.SPECIAL_EVENT_NAME, -1000);
+        discountResult.addDiscount(Values.GIVEAWAY_EVENT_NAME, -25000);
+    }
+
+    @DisplayName("지불 금액을 계산한다")
+    @Test
+    void shouldCalculateCorrectPaymentAmount() {
+        int expectedValue = 135754;
+        PaymentAmountCalculator paymentAmountCalculator = new PaymentAmountCalculator();
+        int paymentAmount = paymentAmountCalculator.calculatePaymentAmount(order, discountResult);
+        assertThat(paymentAmount).isEqualTo(expectedValue);
+    }
+
+}
