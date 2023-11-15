c7db9f8558cfba8e001a8709af390a8e3cc92823
junest66 <chlwndks333@gmail.com>
2023-11-15 19:22:48 +0900

    feat(DateValidator): 0으로 시작하는 날짜형태 예외 발생 기능 추가

--- src/main/java/christmas/validator/DateValidator.java
+++ src/main/java/christmas/validator/DateValidator.java
@@ -5,7 +5,7 @@ import christmas.constants.Values;
 
 public class DateValidator {
     public static void validate(String input) {
-        if (isNotNumber(input) || isDateOutOfRange(Integer.parseInt(input))) {
+        if (isNotNumber(input) || isDateOutOfRange(Integer.parseInt(input)) || startsWithZero(input)) {
             throw new IllegalArgumentException(Messages.INVALID_DATE_MESSAGE);
         }
     }
@@ -21,5 +21,9 @@ public class DateValidator {
 
     private static boolean isDateOutOfRange(int date) {
         return date < Values.MIN_DATE || date > Values.MAX_DATE;
+    }
+
+    private static boolean startsWithZero(String input) {
+        return input.startsWith("0");
     }
 }
