e55a69af7b9e2c7ffe17dc6cd725925c7c2bc7f9
junest66 <chlwndks333@gmail.com>
2023-12-13 16:59:08 +0900

    feat(Coach): 코치 클래스 추가 및 메뉴가 유효한 메뉴인지 판단하는 기능 추가

---
+++ java-menu/src/main/java/menu/domain/Coach.java
@@ -0,0 +1,21 @@
+package menu.domain;
+
+import java.util.List;
+
+public class Coach {
+    private final String name;
+    private final List<String> ineligibleFoods;
+
+    public Coach(String name, List<String> ineligibleFoods) {
+        this.name = name;
+        this.ineligibleFoods = ineligibleFoods;
+    }
+
+    public boolean isMenuEligible(String menu) {
+        return ineligibleFoods.contains(menu);
+    }
+
+    public String getName() {
+        return name;
+    }
+}
