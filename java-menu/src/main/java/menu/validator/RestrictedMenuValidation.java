ef786f52b440ce81f1f948b45145d26c7a33f840
junest66 <chlwndks333@gmail.com>
2023-12-13 17:02:23 +0900

    feat(RestrictedMenuValidation): 못 먹는 음식 입력 유효성 검증 기능 추가

---
+++ java-menu/src/main/java/menu/validator/RestrictedMenuValidation.java
@@ -0,0 +1,34 @@
+package menu.validator;
+
+import java.util.Arrays;
+import java.util.List;
+import menu.constants.FoodMenu;
+
+public class RestrictedMenuValidation {
+    private static final String SEPARATOR = ",";
+    private static final int MINIMUM_RESTRICTED_MENUS = 0;
+    private static final int MAXIMUM_RESTRICTED_MENUS = 2;
+    private static final String MENU_COUNT_ERROR_MESSAGE =
+            "[ERROR] 먹지 못하는 음식의 갯수은 최소 " + MINIMUM_RESTRICTED_MENUS + "개 최대 " + MAXIMUM_RESTRICTED_MENUS + "개 입니다.";
+    private static final String INVALID_MENU_ERROR_MESSAGE = "[ERROR] 없는 메뉴입니다. 다시 입력해주세요.";
+
+    public void validate(String input) {
+        List<String> menuNames = Arrays.asList(input.split(SEPARATOR));
+        checkMenuCount(menuNames);
+        checkValidMenu(menuNames);
+    }
+
+    public void checkMenuCount(List<String> menuNames) {
+        if (menuNames.size() < MINIMUM_RESTRICTED_MENUS || menuNames.size() > MAXIMUM_RESTRICTED_MENUS) {
+            throw new IllegalArgumentException(MENU_COUNT_ERROR_MESSAGE);
+        }
+    }
+
+    public void checkValidMenu(List<String> menuNames) {
+        for (String menuName : menuNames) {
+            if (!FoodMenu.isValidName(menuName)) {
+                throw new IllegalArgumentException(INVALID_MENU_ERROR_MESSAGE);
+            }
+        }
+    }
+}
