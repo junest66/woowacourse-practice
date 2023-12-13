535fba2d91cea70dc34253292f73dfb306ca8766
junest66 <chlwndks333@gmail.com>
2023-12-13 17:08:44 +0900

    feat(ResultRecommendMenu): 코치들에게 메뉴를 추천해 결과를 담는 기능 추가

---
+++ java-menu/src/main/java/menu/domain/ResultRecommendMenu.java
@@ -0,0 +1,39 @@
+package menu.domain;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import menu.constants.DayOfWeek;
+import menu.constants.FoodCategory;
+
+public class ResultRecommendMenu {
+    private final Map<Coach, List<String>> result = new LinkedHashMap<>();
+    private final List<FoodCategory> recommendedCategories = new ArrayList<>();
+
+    public void recommend(List<Coach> coaches) {
+        RecommendationGenerator generator = new RecommendationGenerator();
+        for (int count = 0; count < DayOfWeek.values().length; count++) {
+            FoodCategory randomCategory = generator.getRandomCategory();
+            recommendedCategories.add(randomCategory);
+            generateRecommendationsForCategory(coaches, generator, randomCategory);
+        }
+    }
+
+    private void generateRecommendationsForCategory(List<Coach> coaches, RecommendationGenerator generator,
+                                                    FoodCategory category) {
+        for (Coach coach : coaches) {
+            String recommendedMenu = generator.generateRecommendation(coach, category, result);
+            result.computeIfAbsent(coach, k -> new ArrayList<>()).add(recommendedMenu);
+        }
+    }
+
+    public Map<Coach, List<String>> getResult() {
+        return Collections.unmodifiableMap(result);
+    }
+
+    public List<FoodCategory> getRecommendedCategories() {
+        return Collections.unmodifiableList(recommendedCategories);
+    }
+}
